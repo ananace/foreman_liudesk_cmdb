8b0662a9c1884a860866c9d7ba6752e4e9f9f423
Alexander Olofsson <alexander.olofsson@liu.se>
2024-10-07 16:03:27 +0200

    Fix final rubocop complaint

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,6 +6,6 @@ Rails.autoloaders.each do |autoloader|
     "foreman_liudesk_cmdb" => "ForemanLiudeskCMDB",
     "liudesk_cmdb" => "LiudeskCMDB",
     "liudesk_cmdb_facet" => "LiudeskCMDBFacet",
-    "liudesk_cmdb_hostgroup_facet" => "LiudeskCMDBHostgroupFacet",
+    "liudesk_cmdb_hostgroup_facet" => "LiudeskCMDBHostgroupFacet"
   )
 end
