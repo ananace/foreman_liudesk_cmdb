5ab29648683631291d2b807e4240d025265e30ab
Alexander Olofsson <alexander.olofsson@liu.se>
2025-02-20 15:33:56 +0100

    Improve handling for CMDB hardware search

--- app/interactors/foreman_liudesk_cmdb/sync_asset/sync_hardware/find.rb
+++ app/interactors/foreman_liudesk_cmdb/sync_asset/sync_hardware/find.rb
@@ -12,12 +12,18 @@ module ForemanLiudeskCMDB
         end
 
         def call
-          found = ForemanLiudeskCMDB::API.find_asset(facet.hardware_model_type, **search_params)
+          found = ForemanLiudeskCMDB::API.find_asset(facet.hardware_model_type, **search_params).sort_by do |hw|
+            evaluate_correctness(hw, search_params)
+          end
+
+          if search_params[:bios_uuid]
+            found.reject! { |hw| hw.bios_uuid && hw.bios_uuid.downcase != search_params[:bios_uuid].downcase }
+          end
 
           if found.count > 1
             ::Foreman::Logging
               .logger("foreman_liudesk_cmdb/sync")
-              .warn("#{self.class} found multiple potential hardware assets.")
+              .warn("#{self.class} found multiple potential hardware assets for #{search_params}")
           end
 
           context.hardware = found.first
@@ -33,6 +39,25 @@ module ForemanLiudeskCMDB
 
         def facet
           host.liudesk_cmdb_facet
+        end
+
+        def evaluate_correctness(hardware, search_params)
+          divergence = 0
+          if search_params[:bios_uuid]
+            divergence += 9000 if hardware.bios_uuid&.downcase != search_params[:bios_uuid].downcase
+          else
+            divergence += 0.5 if hardware.bios_uuid.nil? || hardware.bios_uuid.empty?
+          end
+          if search_params[:serial_number]
+            divergence += 50 if hardware.serial_number&.downcase != search_params[:serial_number].downcase
+          else
+            divergence += 0.5 if hardware.serial_number.nil? || hardware.serial_number.empty?
+          end
+          if search_params['macAndNetworkAccessRoles.mac']
+            divergence += 2 if !hardware.mac_and_network_access_roles&.any? { |nic| nic[:mac].downcase == search_params['macAndNetworkAccessRoles.mac'].downcase }
+          end
+          divergence += 1 if hardware.host_name && hardware.host_name.downcase != host.name.downcase
+          divergence
         end
 
         def search_params
